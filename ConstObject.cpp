3b9eb33e5b508a108dd94554ae2798274ad27d57
이인영 <121835105+220in@users.noreply.github.com>
2023-06-19 17:15:39 +0900

    Add files via upload

---
+++ ConstObject.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class SoSimple
+{
+private:
+	int num;
+public:
+	SoSimple(int n) : num(n)
+	{ }
+	SoSimple& AddNum(int n)
+	{
+		num += n;
+		return *this;
+	}
+	void ShowData() const
+	{
+		cout << "num:" << num << endl;
+	}
+};
+
+int main(void)
+{	
+	// const 객체 생성
+	const SoSimple obj(7);
+	// obj.AddNum(20); ==> const 함수가 X
+	obj.ShowData(); 
+	return 0;
+}
